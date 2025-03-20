632494fd02ab4f6149c2d448ebbcb1e9014df28e
devbelek <belekasrarov10@gmail.com>
2025-03-20 16:57:17 +0600

    fix islam

--- config/settings.py
+++ config/settings.py
@@ -95,6 +95,7 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
+    'EXCEPTION_HANDLER': 'rest_framework.views.exception_handler',
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
