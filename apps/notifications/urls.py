ff4288052dd6c11bea8381e7534fb40947da7738
devbelek <belekasrarov10@gmail.com>
2025-03-20 16:51:10 +0600

    fix islam

--- apps/notifications/urls.py
+++ apps/notifications/urls.py
@@ -2,14 +2,13 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import NotificationViewSet, PushSubscriptionViewSet
 
-
-notification_router = DefaultRouter()
-notification_router.register(r'notifications', NotificationViewSet, basename='notification')
+router = DefaultRouter()
+router.register(r'', NotificationViewSet, basename='notification')
 
 push_router = DefaultRouter()
-push_router.register(r'notifications/push-subscriptions', PushSubscriptionViewSet, basename='push-subscription')
+push_router.register(r'push-subscriptions', PushSubscriptionViewSet, basename='push-subscription')
 
 urlpatterns = [
-    path('api/', include(notification_router.urls)),
-    path('api/', include(push_router.urls)),
+    path('', include(router.urls)),
+    path('', include(push_router.urls)),
 ]
