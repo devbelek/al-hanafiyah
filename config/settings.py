7e0b4a4310e08b67654d0d8f44227b5d1355b5aa
devbelek <belekasrarov10@gmail.com>
2025-04-12 22:26:29 +0600

    fd

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ else:
 SECRET_KEY = env('SECRET_KEY')
 DEBUG = env('DEBUG', default=True)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '37.252.23.134']
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'jazzmin',
